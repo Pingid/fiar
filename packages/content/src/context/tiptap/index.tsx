bf455917dd8806449dcc02d986a3d86620900732
Dan Beaven <dm.beaven@gmail.com>
2023-11-07 16:57:57 +0100

    Improve memoization

--- packages/content/src/context/tiptap/index.tsx
+++ packages/content/src/context/tiptap/index.tsx
@@ -1,5 +1,5 @@
 import type { AnyExtension, Editor } from '@tiptap/react'
-import React from 'react'
+import React, { useMemo } from 'react'
 
 type TipTapConfig = {
   extensions: AnyExtension[]
@@ -8,8 +8,13 @@ type TipTapConfig = {
 const TipTapContext = React.createContext<TipTapConfig>({ extensions: [], controls: [] })
 export const TipTapConfigProvider = (p: { children: React.ReactNode } & Partial<TipTapConfig>): JSX.Element => {
   const parent = React.useContext(TipTapContext)
-  const controls = [...parent.controls, ...(p.controls || [])]
-  const extensions = [...parent.extensions, ...(p.extensions || [])]
-  return <TipTapContext.Provider value={{ extensions, controls }}>{p.children}</TipTapContext.Provider>
+  const child = useMemo(
+    () => ({
+      controls: [...parent.controls, ...(p.controls ?? [])],
+      extensions: [...parent.extensions, ...(p.extensions ?? [])],
+    }),
+    [parent, p.controls, p.extensions],
+  )
+  return <TipTapContext.Provider value={child}>{p.children}</TipTapContext.Provider>
 }
 export const useTipTapConfig = () => React.useContext(TipTapContext)
