bd3b5735980d3fe9ca646d867309952b0b986580
Dan Beaven <dm.beaven@gmail.com>
2024-03-20 23:45:33 +0000

    Show collection item id when missing titleField

--- packages/content/src/components/document/title/index.tsx
+++ packages/content/src/components/document/title/index.tsx
@@ -1,16 +1,19 @@
 import { useWatch } from 'react-hook-form'
 
+import { useModel, usePathRef } from '../../../context/model.js'
 import { useFormContext } from '../../../context/field.js'
-import { useModel } from '../../../context/model.js'
 
 export const DocumentFormTitle = () => {
   const model = useModel()
+  const path = usePathRef()
+
   if (model.type === 'collection' && model.titleField) return <TitleField titleField={model.titleField} />
+  if (model.type === 'collection') return path.split('/').slice(-1)[0] || model.label
   return model.label
 }
 
 const TitleField = (props: { titleField: string }) => {
   const form = useFormContext()
-  const watched = useWatch({ control: form.control, name: props.titleField, defaultValue: 'Untitled' })
+  const watched = useWatch({ control: form.control, name: `data.${props.titleField}`, defaultValue: 'Untitled' })
   return watched
 }
